1f6a59f7029266fc73b821c5698466d8c14f1521
Niklas Seyfarth <niklas@seyfarth.de>
2017-12-07 14:57:13 +0100

    Create Chain, ChainPart and ForEachTest.

---
+++ src/de/seyfarth/tutorium/tasks/ChainPart.java
@@ -0,0 +1,23 @@
+package de.seyfarth.tutorium.tasks;
+
+public class ChainPart {
+
+	private final Object content;
+	private ChainPart next;
+	
+	public ChainPart(Object content) {
+		this.content = content;
+	}
+
+	public Object getContent() {
+		return this.content;
+	}
+	
+	public ChainPart getNext() {
+		return this.next;
+	}
+	
+	public void setNext(ChainPart next) {
+		this.next = next;
+	}
+}
---
+++ src/de/seyfarth/tutorium/tasks/ForEachTest.java
@@ -0,0 +1,25 @@
+package de.seyfarth.tutorium.tasks;
+
+public class ForEachTest {
+	public static void main(String[] args) {
+		String[] strings = {"1" , "2", "3", "4"};
+		for (String string : strings) {
+			System.out.print(string + " ");
+		}
+		
+		System.out.println();
+		
+		ChainPart firstChainPart = new ChainPart("1");
+		firstChainPart.setNext(new ChainPart("2"));
+		firstChainPart.getNext().setNext(new ChainPart("3"));
+		firstChainPart.getNext().getNext().setNext(new ChainPart("4"));
+		ChainPart current = firstChainPart;
+		while (current != null) {
+			System.out.print(current.getContent() + " ");
+			current = current.getNext();
+		}
+		
+		System.out.println();
+		
+	}
+}
