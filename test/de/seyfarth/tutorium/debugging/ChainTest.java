b415df6b866d258a9dce9b5334fdc8c6bfe91ebc
Niklas Seyfarth <niklas@seyfarth.de>
2018-01-16 11:38:18 +0100

    Add Generics to ChainTest.

--- test/de/seyfarth/tutorium/debugging/ChainTest.java
+++ test/de/seyfarth/tutorium/debugging/ChainTest.java
@@ -8,32 +8,32 @@ public class ChainTest {
 
     @Test
     public void testSizeIsZero() {
-        Chain chain = new Chain();
+        Chain<Object> chain = new Chain<>();
         assertEquals(0, chain.size());
     }
 
     @Test
     public void testSetEmptyChain() {
-        Chain chain = new Chain();
+        Chain<Object> chain = new Chain<>();
         assertNull(chain.set(0, new Object()));
     }
 
     @Test
     public void testSetEndChain() {
-        Chain chain = new Chain();
+        Chain<Object> chain = new Chain<>();
         chain.add(new Object());
         assertNull(chain.set(1, new Object()));
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testGetIndexMinusOne() {
-        Chain chain = new Chain();
+        Chain<Object> chain = new Chain<>();
         chain.get(-1);
     }
 
     @Test
     public void testSetReplaceFirstElement() {
-        Chain chain = new Chain();
+        Chain<Object> chain = new Chain<>();
         Object expected = new Object();
         chain.add(expected);
         Object replacement = new Object();
